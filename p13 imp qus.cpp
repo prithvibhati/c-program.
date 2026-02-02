35305db70086eacffced9be936066e2aa2dab5c5
prithvi raj <prithvibhati9929@gmail.com>
2026-02-02 21:45:44 +0530

    Add files via upload

---
+++ p13 imp qus.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{ int  add,sub,mul,div,mod;
+  int n,p;
+    printf("enter velue of n&p");
+    scanf("%d%d",&p,&n);
+     add=p+n;
+     scanf("%d",&add);
+       printf("enter subtraction");
+       sub=p-n;
+       printf("%d",sub);
+       printf("enter multiply ");
+       mul=p*n;
+       printf("%d",mul);
+       printf("enter division");
+       div=p/n;
+       printf("%d",div);
+       printf("enter moduler");
+       mod=p%n;
+       printf("%d",mod);
+       
+}
